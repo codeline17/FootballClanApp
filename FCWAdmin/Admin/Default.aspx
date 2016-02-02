4db40dc87c94cc0e9251aae7ef8e38be76b83c95
knopix <ahaviari@gmail.com>
2016-02-02 20:13:17 +0100

    Quand Meme...

---
+++ FCWAdmin/Admin/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FCWAdmin.Admin.Default" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentHead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentTitle" runat="server">
+    Ndeshjet
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentContent" runat="server">
+    <script type="text/javascript">
+        function DateChanged() {
+            $get('<%=txtCalendar.ClientID %>').value = $find('dtNdeshjet').get_selectedDate();
+            $get('<%=btnFilter.ClientID %>').click();
+       }
+    </script>
+    <asp:TextBox ID="txtCalendar" runat="server" OnTextChanged="txtCalendar_TextChanged" ClientIDMode="Static"></asp:TextBox>
+    <asp:Button ID="btnFilter" runat="server" Text="Button" OnClick="btnFilter_Click" ClientIDMode="Static" />
+    <ajaxToolkit:CalendarExtender ClientIDMode="Static" ID="dtNdeshjet" runat="server" Format="dd/MM/yyyy" TargetControlID="txtCalendar" OnClientDateSelectionChanged="DateChanged" />
+    <asp:GridView ID="gdView" Width="50%" AutoGenerateEditButton="true" runat="server" PageSize="50" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="Id" ForeColor="#333333" GridLines="None" OnRowDataBound="gdView_RowDataBound" OnRowEditing="gdView_RowEditing" OnRowCancelingEdit="gdView_RowCancelingEdit" OnRowUpdating="gdView_RowUpdating">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" Visible="false" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:HiddenField ID="hdId" runat="server" Value='<%# Eval("Id") %>' />
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:HiddenField ID="hdId" runat="server" Value='<%# Eval("Id") %>' />
+                </EditItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="HomeId" HeaderText="HomeId" SortExpression="HomeId" Visible="false" />
+            <asp:BoundField DataField="AwayId" HeaderText="AwayId" SortExpression="AwayId" Visible="false" />
+            <asp:BoundField DataField="LeagueId" HeaderText="LeagueId" SortExpression="LeagueId" Visible="false" />
+            <asp:BoundField DataField="StartDate" ReadOnly="true" HeaderText="StartDate" SortExpression="StartDate" DataFormatString="{0:dd/MM/yyyy HH:mm}" />
+            <asp:BoundField DataField="Name" ReadOnly="true" HeaderText="Home Team" SortExpression="Name" />
+            <asp:BoundField DataField="Name1" ReadOnly="true" HeaderText="Away Team" SortExpression="Name1" />
+            <asp:BoundField DataField="Name2" ReadOnly="true" HeaderText="League" SortExpression="Name2" />
+            <asp:BoundField DataField="PackId" HeaderText="Pack" SortExpression="PackId" Visible="false" />
+            <asp:TemplateField HeaderText="Pack" SortExpression="Pack">
+                <ItemTemplate>
+                    <asp:Label ID="lblPack" runat="server" Text='<%# Eval("PackName")%>'></asp:Label>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:Label ID="lblPack" runat="server" Text='<%# Eval("PackName")%>' Visible="false"></asp:Label>
+                    <asp:DropDownList ID="ddlPacks" runat="server" AutoPostBack="True">
+                    </asp:DropDownList>
+                </EditItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+        <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+        <SortedAscendingCellStyle BackColor="#FDF5AC" />
+        <SortedAscendingHeaderStyle BackColor="#4D0000" />
+        <SortedDescendingCellStyle BackColor="#FCF6C0" />
+        <SortedDescendingHeaderStyle BackColor="#820000" />
+    </asp:GridView>
+</asp:Content>
---
+++ FCWAdmin/Admin/Default.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FCWAdmin.Admin
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private SqlDataSource dsMatches = new SqlDataSource();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (dtNdeshjet.SelectedDate == null)
+                {
+                    dtNdeshjet.SelectedDate = DateTime.Now.Date;
+                }
+                BindGrid(dtNdeshjet.SelectedDate);
+            }       
+                 
+        }        
+
+        protected void txtCalendar_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnFilter_Click(object sender, EventArgs e)
+        {
+            var dtStr = Request.Form[txtCalendar.UniqueID];
+            DateTime dt = DateTime.Now;
+            DateTime.TryParseExact(dtStr, "dd/MM/yyyy",CultureInfo.InvariantCulture, DateTimeStyles.None, out dt);
+
+            DateTime time;
+            var matchingCulture = CultureInfo.GetCultures(CultureTypes.AllCultures).FirstOrDefault(ci => DateTime.TryParse(dtStr, ci, DateTimeStyles.None, out time));
+            //DateTime.TryParseExact(dtStr, matchingCulture, DateTimeStyles.None, out dt);
+            BindGrid(Convert.ToDateTime(dtStr));
+        }
+
+        private void BindGrid(DateTime? date)
+        {
+            if (date == null)
+                   return;
+
+            
+            dsMatches.ID = "dsMatches";
+            Page.Controls.Add(dsMatches);
+            dsMatches.ConnectionString = ConfigurationManager.ConnectionStrings["AdminConnectionString"].ConnectionString;
+            dsMatches.SelectCommandType = SqlDataSourceCommandType.StoredProcedure;
+            dsMatches.SelectCommand = "MatchGetByDate";
+            if (dsMatches.SelectParameters.Count == 0)
+            {
+                dsMatches.SelectParameters.Add("startDate", TypeCode.DateTime, ((DateTime)date).ToString("yyyy-MM-dd HH:mm:ss"));
+            }
+            else
+            {
+                dsMatches.SelectParameters["startDate"].DefaultValue = ((DateTime)date).ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            
+            gdView.DataSource = dsMatches;
+            gdView.DataBind();
+
+        }
+
+        protected void gdView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && gdView.EditIndex == e.Row.RowIndex)
+            {
+                DropDownList ddlCities = (DropDownList)e.Row.FindControl("ddlPacks");
+                string query = "select * from FixturePacks";
+                SqlCommand cmd = new SqlCommand(query);
+                ddlCities.DataSource = GetData(cmd);
+                ddlCities.DataTextField = "Name";
+                ddlCities.DataValueField = "Id";
+                if (ddlCities.Items.Count == 0)
+                {
+                    ddlCities.DataBind();
+                    ddlCities.Items.FindByText((e.Row.FindControl("lblPack") as Label).Text).Selected = true;
+                }                
+            }
+        }
+
+        private DataTable GetData(SqlCommand cmd)
+        {
+            string strConnString = ConfigurationManager.ConnectionStrings["AdminConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(strConnString))
+            {
+                using (SqlDataAdapter sda = new SqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+        
+
+        protected void gdView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gdView.EditIndex = e.NewEditIndex;
+            var dtStr = Request.Form[txtCalendar.UniqueID];
+            DateTime dt = DateTime.Now;
+            DateTime.TryParseExact(dtStr, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt);
+            BindGrid(Convert.ToDateTime(dtStr));
+        }
+
+        protected void gdView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {                
+                GridViewRow row = gdView.Rows[e.RowIndex];
+                DropDownList ddlPacks = (DropDownList)row.FindControl("ddlPacks");
+                HiddenField hfId = (HiddenField)row.FindControl("hdId");
+                var fixtureId = hfId.Value;
+
+                dsMatches.ConnectionString = ConfigurationManager.ConnectionStrings["AdminConnectionString"].ConnectionString;
+                dsMatches.UpdateCommandType = SqlDataSourceCommandType.StoredProcedure;
+                dsMatches.UpdateCommand = "FixturePackUpdate";
+                if (dsMatches.UpdateParameters.Count == 0)
+                {
+                    dsMatches.UpdateParameters.Add("fixtureId", DbType.Int32, fixtureId);
+                    dsMatches.UpdateParameters.Add("packId", DbType.Int32, ddlPacks.SelectedValue);
+                    dsMatches.Update();
+                }
+                BindGrid(dtNdeshjet.SelectedDate);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
+
+        }
+
+        protected void gdView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+    }
+}
