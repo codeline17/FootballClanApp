ee58306d4ddef0e88ea3e8957e0e4955ea5c30c6
andi haviari <ahaviari@gmail.com>
2016-04-26 08:11:33 +0200

    big changes [Leli mos e merr]

---
+++ Smaug/Forms/frmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Smaug.Requests;
+using Smaug.Utils;
+
+namespace Smaug
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            GetHighlights();
+
+        }
+        
+        private void GetHighlights()
+        {
+            var c = new MyConsole();
+            c.WriteLine("aa");
+        }
+
+        private void GetExtendedFixtures()
+        {
+            var doc = Feed.GetExtendedFixtures();
+            foreach (var element in doc.Descendants("league"))
+            {
+                
+            }
+        }
+
+        public void AppendText(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(AppendText), new object[] { text });
+                return;
+            }
+            txtConsole.Text += Environment.NewLine + text;
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            gdMain.ResetBindings();
+        }
+    }
+}
---
+++ Smaug/Requests/Highlightscs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Smaug.Utils;
+
+namespace Smaug.Requests
+{
+    public static class Feed
+    {
+        public static XDocument GetHighlights(string days)
+        {
+            //http://www.tipgin.net/datav2/accounts/bsp/soccer/highlights/d-1.xml
+            days = days == "today" ? days : "d" + days;
+
+            return GetXmlFromUrl($"http://www.tipgin.net/datav2/accounts/bsp/soccer/highlights/{days}.xml");
+        }
+
+        public static XDocument GetExtendedFixtures()
+        {
+            return GetXmlFromUrl("http://feed.me/england-demo.xml");
+        }
+
+        private static XDocument GetXmlFromUrl(string url)
+        {
+            var xml = new XDocument();
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("url");
+                request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+                request.Accept = "application/xml";
+
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    var statusCode = (int)response.StatusCode;
+                    if (response == null) return xml;
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        xml = XDocument.Load(reader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+            }
+            
+            return xml;
+        }
+    }
+}
