efa2f7c8c3042d028d1a50ebbaa48820417a393b
ahaviari <ahaviari@gmail.com>
2016-09-01 18:06:10 +0200

    smaug

--- Smaug/Forms/Form1.Designer.cs
+++ Smaug/Forms/Form1.Designer.cs
@@ -31,13 +31,13 @@
             this.components = new System.ComponentModel.Container();
             this.lMain = new System.Windows.Forms.TableLayoutPanel();
             this.txtConsole = new System.Windows.Forms.TextBox();
-            this.gdMain = new System.Windows.Forms.DataGridView();
-            this.tsMain = new System.Windows.Forms.ToolStrip();
-            this.btnRefresh = new System.Windows.Forms.ToolStripButton();
+            this.matchGetByDateBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.samwiseDataSet = new Smaug.SamwiseDataSet();
             this.tmrEFixtures = new System.Windows.Forms.Timer(this.components);
+            this.matchGetByDateTableAdapter = new Smaug.SamwiseDataSetTableAdapters.MatchGetByDateTableAdapter();
             this.lMain.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gdMain)).BeginInit();
-            this.tsMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.matchGetByDateBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.samwiseDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // lMain
@@ -46,8 +46,6 @@
             this.lMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.lMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.lMain.Controls.Add(this.txtConsole, 0, 2);
-            this.lMain.Controls.Add(this.gdMain, 0, 1);
-            this.lMain.Controls.Add(this.tsMain, 0, 0);
             this.lMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lMain.Location = new System.Drawing.Point(0, 0);
             this.lMain.Name = "lMain";
@@ -67,39 +65,25 @@
             this.txtConsole.Size = new System.Drawing.Size(1108, 151);
             this.txtConsole.TabIndex = 0;
             // 
-            // gdMain
+            // matchGetByDateBindingSource
             // 
-            this.gdMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gdMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.gdMain.Location = new System.Drawing.Point(3, 45);
-            this.gdMain.Name = "gdMain";
-            this.gdMain.Size = new System.Drawing.Size(1108, 283);
-            this.gdMain.TabIndex = 1;
+            this.matchGetByDateBindingSource.DataMember = "MatchGetByDate";
+            this.matchGetByDateBindingSource.DataSource = this.samwiseDataSet;
             // 
-            // tsMain
+            // samwiseDataSet
             // 
-            this.tsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnRefresh});
-            this.tsMain.Location = new System.Drawing.Point(0, 0);
-            this.tsMain.Name = "tsMain";
-            this.tsMain.Size = new System.Drawing.Size(1114, 25);
-            this.tsMain.TabIndex = 2;
-            this.tsMain.Text = "toolStrip1";
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.btnRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(23, 22);
-            this.btnRefresh.Text = "Refresh";
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            this.samwiseDataSet.DataSetName = "SamwiseDataSet";
+            this.samwiseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // tmrEFixtures
             // 
             this.tmrEFixtures.Enabled = true;
             this.tmrEFixtures.Interval = 600000;
             this.tmrEFixtures.Tick += new System.EventHandler(this.tmrEFixtures_Tick);
+            // 
+            // matchGetByDateTableAdapter
+            // 
+            this.matchGetByDateTableAdapter.ClearBeforeFill = true;
             // 
             // frmMain
             // 
@@ -111,9 +95,8 @@
             this.Text = "Form1";
             this.lMain.ResumeLayout(false);
             this.lMain.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.gdMain)).EndInit();
-            this.tsMain.ResumeLayout(false);
-            this.tsMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.matchGetByDateBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.samwiseDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,10 +105,10 @@
 
         private System.Windows.Forms.TableLayoutPanel lMain;
         private System.Windows.Forms.TextBox txtConsole;
-        private System.Windows.Forms.DataGridView gdMain;
-        private System.Windows.Forms.ToolStrip tsMain;
-        private System.Windows.Forms.ToolStripButton btnRefresh;
         private System.Windows.Forms.Timer tmrEFixtures;
+        private System.Windows.Forms.BindingSource matchGetByDateBindingSource;
+        private SamwiseDataSet samwiseDataSet;
+        private SamwiseDataSetTableAdapters.MatchGetByDateTableAdapter matchGetByDateTableAdapter;
     }
 }
 
